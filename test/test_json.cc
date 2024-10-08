e752d7aa993d1499a1eb34ae7ff42bfa2da2a4e0
Vietor Liu <vietor.liu@gmail.com>
2024-10-09 02:19:12 +0800

    Add more ctest support

--- test/test_json.cc
+++ test/test_json.cc
@@ -1,6 +1,6 @@
 #include <string>
 
-#include "../src/star_text.h"
+#include "../src/star_json.h"
 #include "my_assert.h"
 
 using std::wstring;
@@ -9,79 +9,96 @@ const wstring keyword_phone(L"phone");
 
 void test_process_false22() {
   StarOptions options{false, 2, 2};
-  StarText engine(options);
+  StarJson engine(options, true);
   engine.AddKeyword((skchar_t*)keyword_phone.data(), keyword_phone.size());
 
-  std::wstring source(L"i am a phone");
-  std::wstring target(L"i am a ph*ne");
+  std::wstring source(L"{\"phone\":\"12345678\"}");
+  std::wstring target(L"{\"phone\":\"12****78\"}");
   engine.ProcessBuffer((skchar_t*)source.data(), source.size());
   MY_ASSERT_ON(source.compare(target) == 0);
 }
 
 void test_process_false20() {
   StarOptions options{false, 2, 0};
-  StarText engine(options);
+  StarJson engine(options, true);
   engine.AddKeyword((skchar_t*)keyword_phone.data(), keyword_phone.size());
 
-  std::wstring source(L"i am a phone");
-  std::wstring target(L"i am a ph***");
+  std::wstring source(L"{\"phone\":\"12345678\"}");
+  std::wstring target(L"{\"phone\":\"12******\"}");
   engine.ProcessBuffer((skchar_t*)source.data(), source.size());
   MY_ASSERT_ON(source.compare(target) == 0);
 }
 
 void test_process_false02() {
   StarOptions options{false, 0, 2};
-  StarText engine(options);
+  StarJson engine(options, true);
   engine.AddKeyword((skchar_t*)keyword_phone.data(), keyword_phone.size());
 
-  std::wstring source(L"i am a phone");
-  std::wstring target(L"i am a ***ne");
+  std::wstring source(L"{\"phone\":\"12345678\"}");
+  std::wstring target(L"{\"phone\":\"******78\"}");
   engine.ProcessBuffer((skchar_t*)source.data(), source.size());
   MY_ASSERT_ON(source.compare(target) == 0);
 }
 
 void test_process_true11() {
   StarOptions options{true, 1, 1};
-  StarText engine(options);
+  StarJson engine(options, true);
   engine.AddKeyword((skchar_t*)keyword_phone.data(), keyword_phone.size());
 
-  std::wstring source(L"i am a PhoNE");
-  std::wstring target(L"i am a P***E");
+  std::wstring source(L"{\"PhoNE\":\"12345678\"}");
+  std::wstring target(L"{\"PhoNE\":\"1******8\"}");
   engine.ProcessBuffer((skchar_t*)source.data(), source.size());
   MY_ASSERT_ON(source.compare(target) == 0);
 }
 
 void test_process_true00() {
   StarOptions options{true, 0, 0};
-  StarText engine(options);
+  StarJson engine(options, true);
   engine.AddKeyword((skchar_t*)keyword_phone.data(), keyword_phone.size());
 
-  std::wstring source(L"i am a PhoNE");
-  std::wstring target(L"i am a *****");
+  std::wstring source(L"{\"PhoNE\":\"12345678\"}");
+  std::wstring target(L"{\"PhoNE\":\"********\"}");
   engine.ProcessBuffer((skchar_t*)source.data(), source.size());
   MY_ASSERT_ON(source.compare(target) == 0);
 }
 
 void test_process_true21() {
   StarOptions options{true, 2, 1};
-  StarText engine(options);
+  StarJson engine(options, true);
   engine.AddKeyword((skchar_t*)keyword_phone.data(), keyword_phone.size());
 
-  std::wstring source(L"i am a PhoNE");
-  std::wstring target(L"i am a Ph**E");
+  std::wstring source(L"{\"PhoNE\":\"12345678\"}");
+  std::wstring target(L"{\"PhoNE\":\"12*****8\"}");
   engine.ProcessBuffer((skchar_t*)source.data(), source.size());
+
   MY_ASSERT_ON(source.compare(target) == 0);
 }
 
 void test_ignore_true00() {
-  StarOptions options{true, 0, 0};
-  StarText engine(options);
+  StarOptions options{true, 2, 1};
+  StarJson engine(options, true);
   engine.AddKeyword((skchar_t*)keyword_phone.data(), keyword_phone.size());
 
-  std::wstring source(L"i am a mobile");
-  std::wstring target(L"i am a mobile");
+  std::wstring source(L"{\"mobile\":\"12345678\"}");
+  std::wstring target(L"{\"mobile\":\"12345678\"}");
   engine.ProcessBuffer((skchar_t*)source.data(), source.size());
   MY_ASSERT_ON(source.compare(target) == 0);
+}
+
+void test_process_number_false22() {
+  StarOptions options{false, 2, 2};
+  StarJson engine(options, false);
+  engine.AddKeyword((skchar_t*)keyword_phone.data(), keyword_phone.size());
+
+  std::wstring source1(L"{\"phone\": 12345678}");
+  std::wstring target1(L"{\"phone\": 12****78}");
+  engine.ProcessBuffer((skchar_t*)source1.data(), source1.size());
+  MY_ASSERT_ON(source1.compare(target1) == 0);
+
+  std::wstring source2(L"{\"phone\": -}");
+  std::wstring target2(L"{\"phone\": *}");
+  engine.ProcessBuffer((skchar_t*)source2.data(), source2.size());
+  MY_ASSERT_ON(source2.compare(target2) == 0);
 }
 
 int main(int argc, char* argv[]) {
@@ -92,6 +109,8 @@ int main(int argc, char* argv[]) {
   test_process_true00();
   test_process_true21();
   test_ignore_true00();
+
+  test_process_number_false22();
 
   return MY_ASSERT_RESULT();
 }
