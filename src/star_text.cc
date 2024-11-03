43556775671151bc0496bc5ce46b8bdc37003e69
Vietor Liu <vietor.liu@gmail.com>
2024-11-03 22:27:04 +0800

    Clean code

--- src/star_text.cc
+++ src/star_text.cc
@@ -3,16 +3,17 @@
 StarText::StarText(const StarOptions& options) : StarBase(options) {}
 
 bool StarText::ProcessBuffer(skchar_t* buffer, size_t length) {
-  size_t index = 0;
   TrieFound found{};
   StarContext context{};
-  while (index < length) {
-    if (!tree_.SearchWord(found, buffer, index, length)) {
+
+  size_t pos = 0;
+  while (pos < length) {
+    if (!tree_.SearchWord(found, buffer, pos, length)) {
       break;
     }
 
+    pos = found.stop_index;
     StarBuffer(context, buffer, found.start_index, found.stop_index);
-    index = found.stop_index;
   }
 
   return context.process_count > 0;
