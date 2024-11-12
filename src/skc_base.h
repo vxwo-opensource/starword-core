7289e42608003cbde9a1af7aa3abf1d76093df0f
Vietor Liu <vietor.liu@gmail.com>
2024-11-12 14:33:13 +0800

    Use uint*_t for skchar_t

--- src/skc_base.h
+++ src/skc_base.h
@@ -1,12 +1,12 @@
 #ifndef SKC_BASE_H_
 #define SKC_BASE_H_
 
-#include <uchar.h>
+#include <stdint.h>
 #ifdef SKC_USE_UTF32
-typedef char32_t skchar_t;
+typedef uint32_t skchar_t;
 #else
 #define SKC_USE_UTF16
-typedef char16_t skchar_t;
+typedef uint16_t skchar_t;
 #endif
 
 #endif
