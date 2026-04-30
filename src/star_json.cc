0569d8b3a249148c5ad9f0894a5f399d4c97df0f
Vietor Liu <vietor.liu@gmail.com>
2026-04-30 14:43:18 +0800

    Clean a weak code

--- src/star_json.cc
+++ src/star_json.cc
@@ -22,7 +22,8 @@ static size_t BufferSkipChar(const skchar_t* buffer, skchar_t ch,
 static size_t BufferForwardSkipChar(const skchar_t* buffer, skchar_t ch,
                                     size_t start_index, size_t stop_index) {
   size_t pos = stop_index;
-  while (pos >= start_index && buffer[pos] == ch) {
+  size_t min_index = start_index == 0 ? 1 : start_index;
+  while (pos >= min_index && buffer[pos] == ch) {
     --pos;
   }
   return stop_index - pos;
